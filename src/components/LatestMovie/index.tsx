27211af2ac8537249aa21401cd54f70ed9951d5c
Syond <syond94@gmail.com>
2022-05-24 23:19:24 -0300

    Revert "implement codestyle (#26)"
    This reverts commit 4df54edeadaf27dec7195849ff956f654ec8b17a.

--- src/components/LatestMovie/index.tsx
+++ src/components/LatestMovie/index.tsx
@@ -1,76 +1,78 @@
 /**
- *
+ * 
  * Tem que ver se vai precisar disso aqui ainda.
  * Não está sendo utizado em nenhum lugar.
- *
+ * 
  */
 
-import React, { useState, useEffect } from 'react';
 
-import { apiGetRequest } from '../../services/movies.service';
 
-import { Spinner } from 'react-bootstrap';
-import './styles.css';
+import React, { useState, useEffect } from "react";
+
+import { apiGetRequest } from "../../services/movies.service";
+
+import { Spinner } from "react-bootstrap";
+import "./styles.css";
 
 interface IMovie {
-	id?: string;
-	adult: false;
-	original_title?: string;
-	poster_path?: string;
+  id?: string;
+  adult: false;
+  original_title?: string;
+  poster_path?: string;
 }
 
 const LatestMovie: React.FC = () => {
-	const [latestMovie, setLatestMovie] = useState<IMovie>();
-	const [loadingLatestMovie, setLoadingLatestMovie] = useState<Boolean>(false);
+  const [latestMovie, setLatestMovie] = useState<IMovie>();
+  const [loadingLatestMovie, setLoadingLatestMovie] = useState<Boolean>(false);
 
-	async function getLatestMovie() {
-		setLoadingLatestMovie(true);
+  async function getLatestMovie() {
+    setLoadingLatestMovie(true);
 
-		const dataToRequest = {
-			action: 'movie',
-			type: 'latest',
-			page: 1,
-		};
+    const dataToRequest = {
+      action: "movie",
+      type: "latest",
+      page: 1,
+    };
 
-		try {
-			const response = await apiGetRequest(dataToRequest);
+    try {
+      const response = await apiGetRequest(dataToRequest);
 
-			setLatestMovie(response.data);
+      setLatestMovie(response.data);
 
-			setLoadingLatestMovie(false);
-		} catch (error: any) {
-			throw new Error(error);
-		}
-	}
+      setLoadingLatestMovie(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	useEffect(() => {
-		getLatestMovie();
-	}, []);
+  useEffect(() => {
+    getLatestMovie();
+  }, []);
 
-	return (
-		<div id="latestmovie-container">
-			<strong>Latest Movie</strong>
-			{loadingLatestMovie ? (
-				<Spinner animation="border" role="status" />
-			) : (
-				<ul className="movies-grid">
-					<li key={latestMovie?.id}>
-						<img
-							alt="poster"
-							src={
-								latestMovie?.poster_path
-									? 'https://image.tmdb.org/t/p/w200' + latestMovie?.poster_path
-									: ''
-							}
-						/>
-						<div className="movie-info">
-							<span>{latestMovie?.original_title}</span>
-						</div>
-					</li>
-				</ul>
-			)}
-		</div>
-	);
+  return (
+    <div id="latestmovie-container">
+      <strong>Latest Movie</strong>
+      {loadingLatestMovie ? (
+        <Spinner animation="border" role="status" />
+      ) : (
+        <ul className="movies-grid">
+          <li key={latestMovie?.id}>
+            <img
+              alt="poster"
+              src={
+                latestMovie?.poster_path
+                  ? "https://image.tmdb.org/t/p/w200" + latestMovie?.poster_path
+                  : ""
+              }
+            />
+            <div className="movie-info">
+              <span>{latestMovie?.original_title}</span>
+            </div>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default LatestMovie;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,160 +1,164 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 
-import { IMovie } from '../../interfaces';
-import { apiGetRequest } from '../../services/movies.service';
+import { IMovie } from "../../interfaces";
+import { apiGetRequest } from "../../services/movies.service";
 
-import { Spinner } from 'react-bootstrap';
-import notFoundImg from '../../assets/images/error-404.jpg';
-import './styles.css';
+import { Spinner } from "react-bootstrap";
+import notFoundImg from "../../assets/images/error-404.jpg";
+import "./styles.css";
 
-import Layout from '../../components/Layout';
-import Header from '../../components/Header';
-import Main from '../../components/Main';
-import SearchInput from '../../components/SearchInput';
-import List from '../../components/List';
-import Button from '../../components/Button';
+import Layout from "../../components/Layout";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+import SearchInput from "../../components/SearchInput";
+import List from "../../components/List";
+import Button from "../../components/Button";
 
 const Home = () => {
-	const [nowPlayingMovies, setNowPlayingMovies] = useState<IMovie[]>([]);
-	const [nextPageNumber, setnextPageNumber] = useState<number>(1);
-	const [loadingNowPlayingMovies, setLoadingNowPlayingMovies] =
-		useState<Boolean>(false);
-	const [searchedMovie, setSearchedMovie] = useState<IMovie[]>([]);
-	const [otherInfos, setotherInfos] = useState<IMovie>();
-	const [isLoadingSearchedMovie, setisLoadingSearchedMovie] =
-		useState<Boolean>();
+  const [nowPlayingMovies, setNowPlayingMovies] = useState<IMovie[]>([]);
+  const [nextPageNumber, setnextPageNumber] = useState<number>(1);
+  const [
+    loadingNowPlayingMovies,
+    setLoadingNowPlayingMovies,
+  ] = useState<Boolean>(false);
+  const [searchedMovie, setSearchedMovie] = useState<IMovie[]>([]);
+  const [otherInfos, setotherInfos] = useState<IMovie>();
+  const [
+    isLoadingSearchedMovie,
+    setisLoadingSearchedMovie,
+  ] = useState<Boolean>();
 
-	async function handleSearch(value: string) {
-		setisLoadingSearchedMovie(true);
+  async function handleSearch(value: string) {
+    setisLoadingSearchedMovie(true);
 
-		const dataToRequest = {
-			action: 'movie',
-			search: true,
-			query: value,
-		};
+    const dataToRequest = {
+      action: "movie",
+      search: true,
+      query: value,
+    };
 
-		try {
-			const movie = await apiGetRequest(dataToRequest);
+    try {
+      const movie = await apiGetRequest(dataToRequest);
 
-			const movieData = movie.data;
-			const movieResults = movie.data.results;
+      const movieData = movie.data;
+      const movieResults = movie.data.results;
 
-			setSearchedMovie(movieResults);
-			setotherInfos(movieData);
+      setSearchedMovie(movieResults);
+      setotherInfos(movieData);
 
-			setisLoadingSearchedMovie(false);
-		} catch (error: any) {
-			throw new Error(error);
-		}
-	}
+      setisLoadingSearchedMovie(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	const getNowPlayingMovies = useRef(() => {});
+  const getNowPlayingMovies = useRef(() => { });
 
-	getNowPlayingMovies.current = async () => {
-		setLoadingNowPlayingMovies(true);
+  getNowPlayingMovies.current = async () => {
+    setLoadingNowPlayingMovies(true);
 
-		const dataToRequest = {
-			action: 'movie',
-			type: 'now_playing',
-			page: nextPageNumber,
-		};
+    const dataToRequest = {
+      action: "movie",
+      type: "now_playing",
+      page: nextPageNumber,
+    };
 
-		try {
-			const response = await apiGetRequest(dataToRequest);
+    try {
+      const response = await apiGetRequest(dataToRequest);
 
-			const nextDataToRequest = [...nowPlayingMovies, ...response.data.results];
+      const nextDataToRequest = [...nowPlayingMovies, ...response.data.results];
 
-			setNowPlayingMovies(nextDataToRequest);
+      setNowPlayingMovies(nextDataToRequest);
 
-			setnextPageNumber(response.data.page + 1);
+      setnextPageNumber(response.data.page + 1);
 
-			setLoadingNowPlayingMovies(false);
-		} catch (error: any) {
-			throw new Error(error);
-		}
-	};
+      setLoadingNowPlayingMovies(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	useEffect(() => {
-		getNowPlayingMovies.current();
-	}, []);
+  useEffect(() => {
+    getNowPlayingMovies.current();
+  }, []);
 
-	return (
-		<Layout>
-			<Header title="Welcome to Awesome Movies">
-				<SearchInput
-					onChange={(e) => {
-						if (e.target.value !== '') {
-							handleSearch(e.target.value);
-						} else {
-							setSearchedMovie([]);
-							setotherInfos({});
-						}
-					}}
-				/>
-			</Header>
+  return (
+    <Layout>
+      <Header title="Welcome to Awesome Movies">
+        <SearchInput
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              handleSearch(e.target.value);
+            } else {
+              setSearchedMovie([]);
+              setotherInfos({});
+            }
+          }}
+        />
+      </Header>
 
-			<Main>
-				<div id="results-info">
-					<span className="total-pages">
-						<strong>Total pages:</strong>
-						<span>{otherInfos?.total_pages}</span>
-					</span>
-					<span className="total-results">
-						<strong>Total results:</strong>
-						<span>{otherInfos?.total_results}</span>
-					</span>
-				</div>
-				{isLoadingSearchedMovie ? (
-					<Spinner animation="border" role="status" />
-				) : (
-					<>
-						<List title="Now playing in theaters">
-							{searchedMovie.length !== 0 ? (
-								searchedMovie.map((result) => (
-									<Link to={`/movie/${result.id}`} key={result.id}>
-										<li>
-											<img
-												alt="poster"
-												src={`
-                          ${
-														result.poster_path
-															? 'https://image.tmdb.org/t/p/w200' +
-															  result.poster_path
-															: notFoundImg
-													}
+      <Main>
+        <div id="results-info">
+          <span className="total-pages">
+            <strong>Total pages:</strong> 
+            <span>{otherInfos?.total_pages}</span>
+          </span>
+          <span className="total-results">
+            <strong>Total results:</strong> 
+            <span>{otherInfos?.total_results}</span>
+          </span>
+        </div>
+        {isLoadingSearchedMovie ? (
+          <Spinner animation="border" role="status" />
+        ) : (
+            <>
+              <List title="Now playing in theaters">
+                {searchedMovie.length !== 0 ? (
+                  searchedMovie.map((result) => (
+                    <Link to={`/movie/${result.id}`}>
+                      <li key={result.id}>
+                        <img
+                          alt="poster"
+                          src={`
+                          ${result.poster_path
+                              ? "https://image.tmdb.org/t/p/w200" +
+                              result.poster_path
+                              : notFoundImg
+                            }
                           `}
-											/>
-										</li>
-									</Link>
-								))
-							) : loadingNowPlayingMovies ? (
-								<Spinner animation="border" role="status" />
-							) : (
-								nowPlayingMovies.map((movie) => (
-									<Link key={movie.id} to={`/movie/${movie.id}`}>
-										<li key={movie.id}>
-											<img
-												alt="poster"
-												src={
-													'https://image.tmdb.org/t/p/w200' + movie.poster_path
-												}
-											/>
-										</li>
-									</Link>
-								))
-							)}
-						</List>
-						<Button
-							name="+"
-							className="load-more"
-							onClick={getNowPlayingMovies.current}
-						/>
-					</>
-				)}
-			</Main>
-		</Layout>
-	);
+                        />
+                      </li>
+                    </Link>
+                  ))
+                ) : loadingNowPlayingMovies ? (
+                  <Spinner animation="border" role="status" />
+                ) : (
+                      nowPlayingMovies.map((movie) => (
+                        <Link key={movie.id} to={`/movie/${movie.id}`}>
+                          <li key={movie.id}>
+                            <img
+                              alt="poster"
+                              src={
+                                "https://image.tmdb.org/t/p/w200" + movie.poster_path
+                              }
+                            />
+                          </li>
+                        </Link>
+                      ))
+                    )}
+              </List>
+              <Button
+                name="+"
+                className="load-more"
+                onClick={getNowPlayingMovies.current}
+              />
+
+            </>
+          )}
+      </Main>
+    </Layout>
+  );
 };
 
 export default Home;
